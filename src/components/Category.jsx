ebb1e77b486a49e698b227a058121f7390074fb9
Erik <eait84344@gmail.com>
2023-12-04 22:55:00 +0400

    Category is read

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,8 +3,40 @@ import '../css/category.css'
 
 export default function Category() {
   return (
-    <div>
+    <div className='main-category'>
+      <div className="main-category__container">
+        <div className="item-container">
+          <a href="#" className="main-category__item border">
+            <img src="https://want.am/images/thumbnails/70/70/detailed/5/68292-1.jpeg.webp" alt="woman" />
 
+            <div>
+              <div className="main-category__item-title">Կանանց նվերներ</div>
+              <div className="main-category__item-kolvo">216 իր</div>
+              <div className="main-category__item-price">Սկսած <span>3900ԴՐ</span></div>
+            </div>
+          </a>
+
+          <a href="#" className="main-category__item border">
+            <img src="https://want.am/images/thumbnails/70/70/detailed/2/71102-1.jpeg.webp" alt="man" />
+
+            <div>
+              <div className="main-category__item-title">Տղամարդկանց նվերներ</div>
+              <div className="main-category__item-kolvo">308 իր</div>
+              <div className="main-category__item-price">Սկսած <span>3000ԴՐ</span></div>
+            </div>
+          </a>
+
+          <a href="#" className="main-category__item">
+            <img src="https://want.am/images/thumbnails/70/70/detailed/6/U0437307_big.jpeg.webp" alt="dodik" />
+
+            <div>
+              <div className="main-category__item-title">Հոբբի</div>
+              <div className="main-category__item-kolvo">1382 իր</div>
+              <div className="main-category__item-price">Սկսած <span>3900ԴՐ</span></div>
+            </div>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
