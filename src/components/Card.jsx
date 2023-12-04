a1a4278e7d42cef4e6d6af2f1f69b11e8958ac84
Erik <eait84344@gmail.com>
2023-12-04 22:59:13 +0400

    cards start

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Cards from './Cards.jsx'
+import '../css/Card.css'
+
+export default function Card() {
+  return (
+    <main className='main'>
+      <Cards />
+    </main>
+  )
+}
---
+++ src/components/Cards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/cards.css'
+
+export default function Cards() {
+  return (
+    <div>
+
+    </div>
+  )
+}
