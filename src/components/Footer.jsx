44df3ef9cc247316b35961768d5dc317e9952722
Erik <eait84344@gmail.com>
2023-12-05 15:28:29 +0400

    footer s top is read

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,18 @@ import '../css/footer.css'
 export default function Footer() {
   return (
     <footer className='footer'>
-
+      <div className="footer-top">
+        <div className="all-footer-top-container">
+          <div className="ty-content">
+            <i class='bx bx-envelope'></i>
+            <h3>Բաժանորդագրվել զեղչերի</h3>
+          </div>
+          <div className="form-content">
+            <input placeholder='Էլ․ փոստ' type="text" id='text' />
+            <label htmlFor="text">SUBSCRIBE</label>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
