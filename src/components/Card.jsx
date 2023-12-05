ed779689439436d5c4e4e63a830f4debb65bfa5d
Erik <eait84344@gmail.com>
2023-12-05 14:54:50 +0400

    all products db is read

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -28,6 +28,129 @@ export default function Card() {
       title: 'Սենսորային լուսամփոփ «Նապաստակ»',
       price: 11900
     }
+  ];
+
+  let allProducts = [
+    {
+      id: 1,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/mmoey9mg9n9315t2z2kos5xv2y9xvylj.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 16900
+    },
+    {
+      id: 2,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/igroved_harry-potter_01.jpeg.webp',
+      title: '',
+      price: 27900
+    },
+    {
+      id: 3,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/untitled-3.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 12900
+    },
+    {
+      id: 4,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/lifestyleltd-7-chudes-duel-panteon-02.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 13900
+    },
+    {
+      id: 5,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/8a9786dc2100ec3b5ba3665a8afd452a.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 19900
+    },
+    {
+      id: 6,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/101276905_0.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 10500
+    },
+    {
+      id: 7,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/101294965_0.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 16500
+    },
+    {
+      id: 8,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/101294965_0.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 1900
+    },
+    {
+      id: 9,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/6012677165.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 14900
+    },
+    {
+      id: 10,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/d29264186652l.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 5500
+    },
+    {
+      id: 11,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/wyjKEAvKnt0.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 27500
+    },
+    {
+      id: 12,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/lifestyleltd-katamino-travel-01.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 11900
+    },
+    {
+      id: 13,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/101294972_0.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 19500
+    },
+    {
+      id: 14,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/Pathfinder_2th_ed_00-1200x800.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 22500
+    },
+    {
+      id: 15,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/TheWitcher_TRPG_CoreBook-1200x800.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 12900
+    },
+    {
+      id: 16,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/tigr_00-1200x800.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 8900
+    },
+    {
+      id: 17,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/15/6730537579.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 13000
+    },
+    {
+      id: 18,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/14/Path_of_the_Serpent_00-1200x800.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 33500
+    },
+    {
+      id: 19,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/14/dune_rulebook_00-1200x800.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 17500
+    },
+    {
+      id: 20,
+      img: 'https://want.am/images/thumbnails/400/400/detailed/14/dune_rulebook_00-1200x800.jpeg.webp',
+      title: 'Քվեստբոքս.Կամելոտի փորձությունը',
+      price: 16900
+    }
   ]
 
   return (
@@ -41,6 +164,10 @@ export default function Card() {
             return <Cards src={items.img} title={items.title} price={items.price} key={items.id} />
           })
         }
+      </div>
+
+      <div className="wa-container">
+        <div className="grid-list"></div>
       </div>
     </main>
   )
